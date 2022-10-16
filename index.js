00bff3709ee586d731b1ebd813f46917688ae22e
Abena Adu <abeanadu@gmail.com>
2022-10-16 12:54:48 -0400

    commit tree 8540eb9374cff979301d0f15a3484cc8a00f9292

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
+function multiply (num1, num2) {
+    return num1 * num2
+}
+let random = Math.ceil(Math.random()) + 1;
+console.log(random);
 
-// Write your code here
+const num3 = 20;
+const num4 = 8;
+function mod(num3, num4) {
+    return num3 % num4;
+}
+const max =  Math.max(1, 2, 4, 20);
